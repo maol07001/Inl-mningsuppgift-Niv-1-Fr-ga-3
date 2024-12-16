329dd49e28fb210e2fb39baa2718a50986072315
mariu <mariu@DESKTOP-CT70FK7>
2024-12-16 16:22:06 +0100

    Add project files.

---
+++ Inlämningsuppgift-Nivå 1 Fråga 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0; // Variabel för summan
+
+            Console.WriteLine("Skriv in 5 heltal:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Tal {i + 1}: ");
+                int nummer = int.Parse(Console.ReadLine());
+                sum += nummer; // sum = sum + nummer
+            }
+            Console.WriteLine("Summan av de fem talen är:"+sum);
+        }
+    }
+}
